4a9156a6db87ada4273876d31e931c3a29b39681
shunsuke <jirudore@gmail.com>
2019-09-22 01:07:02 +0900

    Project詳細ページのAPI作成

---
+++ app/Http/Controllers/ProjectDetailController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Project;
+
+class ProjectDetailController extends Controller
+{
+
+    // project_detailブレードを返す処理
+    public function index()
+    {
+        return view('project_detail');
+    }
+    
+    // クリックされた画像の詳細データをJSONで返す処理
+    public function select(int $project_id)
+    {
+      return Project::with([
+        'project_images.tags',
+        'project_images.products.tags',
+        'project_images.products.product_images',
+        'project_images.products.brands',
+        ])->find($project_id);
+    }
+}
